fea02ef067ac00b94d8cea6818849e793c771973
nitishat7 <nitishatimaslina7@gmail.com>
2023-05-23 21:50:10 +0545

    first python program

---
+++ operators.py
@@ -0,0 +1,63 @@
+
+#
+# # variable name
+#
+# # num1 = 22
+# # num2 = 33
+# # print("num1+num2", num1+num2)
+#
+# # program for movie expense
+# # print("Welcome to Movie Mania!!")
+# # ticket = 450
+# # popcorn = 350
+# # drinks = 150
+# # snacks = 250
+# # total = ticket + popcorn + drinks + snacks
+# # print("Total amount is = ", total)
+# # pay_amount = 2000
+# # return_amount = pay_amount - total
+# # print("Return = ", return_amount)
+#
+#
+# momo = 300
+# drinks = 100
+# total = momo + drinks
+# print(" Total amount is ", total)
+# pay_amount = 1000
+# ret_amt = pay_amount - total
+# print("Payment :", ret_amt)
+#
+# pani = 1200
+# batti = 1500
+# rent = 10000
+# internet = 1500
+# kitchen = 12000
+# total = batti + pani + rent + internet + kitchen
+# print("Total Budget per month", total)
+# print("Total Budget per Year", total * 12)
+#
+#
+# # print("")
+# # n1 = 2
+# # n2 = 3
+# # n3 = 2
+# # res = n1 + n2 + n3
+# # res = n1 - n2 - n3
+# # res = n1 * n2 * n3
+# # print("Result", res)
+#
+# # add income and calculate the saving
+# print("")
+# print("")
+# income = 12000
+# expense1 = 1200
+# expense2 = 1500
+# expense3 = 10000
+# expense4 = 1500
+# expense5 = 12000
+# total_exp = expense1+expense2+expense3+expense4+expense5
+# print("Total Expenses = ", total_exp)
+# total_savings = income - total_exp
+# print("Total Saving = ", total_savings)
+#
+
