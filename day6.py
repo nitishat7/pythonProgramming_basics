b60c534502decc3191b72c8a203c10fbf9f5b3fd
nitishat7 <nitishatimaslina7@gmail.com>
2023-05-26 21:22:00 +0545

    Looping

--- day6.py
+++ day6.py
@@ -1,16 +1,18 @@
-print("Hello")
-mark1 = float(input("Enter the mark1 "))
-mark2 = float(input("Enter the mark2 "))
-mark3 = float(input("Enter the mark3 "))
-total = mark1 + mark2 + mark3
-per = total / 3
-if mark1 <= 100 and mark2 <= 100 and mark3 <= 100:
-    print("Scored percentage is", per)
-    if per >= 80:
-        print("Distinction")
-        if per >= 90:
-            print("Babbal vayecha")
-    if per < 80:
-        print("Try Again!")
+a = int(input("Enter any number:"))
+if a != 0:
+    if a % 10 == 0:
+        print("Entered number is even")
+    else:
+        print("Entered number is odd number")
 else:
-    print("Wrong Input")
+    print("Entered number is zero")
+
+
+# print("Hello")
+# print("Hello")
+# print("Hello")
+# print("Hello")
+# print("Hello")
+
+
+
---
+++ loop.py
@@ -0,0 +1,66 @@
+# # Program 1
+# a = 2 < 5
+# print(a)
+#
+# while a:
+#     print("Hello", end=' ')
+#
+#
+# a = 3
+# while a <= 10:
+#     print(a, end=' ')
+#     a = a + 1
+#
+# a = 50
+# while a <= 60:
+#     print(a, '', end='')
+#     a = a + 1
+#
+# a = int(input("Enter a number"))
+# num = int(input("Enter end point"))
+# update = int(input("Enter the update value"))
+# while a <= num:
+#
+# num1 = int(input("Enter first number:"))
+# num2 = int(input("Enter second number:"))
+# if num1 > num2:
+#     print("first number is larger")
+# elif num1 == num2:
+#     print("Both numbers are equal")
+# else:
+#     print("second number is larger")
+
+# i = 1
+# while i <= 3:
+#     nationality = input("Enter your Nationality :")
+#     if nationality == "Nepali":
+#         print("Matched")
+#     elif nationality == "bidesi":
+#         print("Thikai Cha")
+#     else:
+#         print("Mars is there")
+#         i += 1
+
+
+# scenario : bank queue
+
+i = 0
+while i <= 5:
+    emp = input("Enter your token number:")
+    if emp <= "10":
+        print("Proceed to counter 1")
+    elif "10" < emp <= "50":
+        print("Proceed to counter 2")
+    else:
+        print("Proceed to counter 3")
+    i = i + 1
+
+
+
+
+
+
+
+
+
+
