56f85bc4262159a5f1f4b2872619c199bf280fd0
nitishat7 <nitishatimaslina7@gmail.com>
2023-05-28 20:13:06 +0545

    Day 7 Updated

---
+++ day7.py
@@ -0,0 +1,91 @@
+#
+# i = 0
+# while i <= 3:
+#     mark1 = float(input("Enter your marks :"))
+#     mark2 = float(input("Enter your marks :"))
+#     total = mark1 + mark2
+#     percentage = total / 2
+#     print("Scored percentage", percentage)
+#     if percentage >= 32:
+#         print("Passes!!")
+#     else:
+#         print("Failed")
+# i = i+1
+
+# multiplication table
+# i = 1
+# while i <= 10:
+#     value = 5*i
+#     print(5, "*", i, "=", value)
+#     i = i + 1
+
+# i = 1
+# num = int(input("Enter the number:"))
+# while i <= 10:
+#     value = num * i
+#     print(num, "*", i, "=", value)
+#     i = i + 1
+
+# nested loop
+
+# i = 1
+# j = 1
+# while j <= 3:
+#     i = 1
+#     num = int(input("Enter the number:"))
+#     while i <= 10:
+#         value = num * i
+#         print(num, "*", i, "=", value)
+#         i = i + 1
+#     j = j + 1
+
+
+# nested loop
+
+
+i = 1
+
+while i <= 10:
+    j = 1
+    while j <= 10:
+        k = 1
+        j = j + 1
+        while k <= 10:
+            k = i * j
+            print(i, "*", j, "=", k)
+        k = k+1
+i = i + 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ nestedloopsandworks.py
@@ -0,0 +1,16 @@
+# 1 to generate multiplication table from 1 to 10
+
+# i = 1
+# while i <= 10:
+#     j = 1
+#
+
+# check whether entered number is odd or even number
+
+num1 = int(input("Enter a number:"))
+# num2 = int(input("Enter a number:"))
+i = 1
+while i<=3:
+    if num1 % 2 == 0:
+        print(num1, "Is Even number  ")
+
